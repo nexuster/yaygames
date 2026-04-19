4ebe136609acefaf1d579ba4521c8d0ac5f1fd3a
nexuster <coralpinklmao@outlook.com>
2026-04-19 13:50:43 -0400

    j

--- console/app.js
+++ console/app.js
@@ -6,8 +6,8 @@ const games = [
 function display_games() {
     const gameContainer = document.getElementById("game_browser");
     
-    for (const game of games) {
-        const div = document.createElement("div");
+    for (let game of games) {
+        var div = document.createElement("div");
         div.className = "app";
         div.style.backgroundImage = `url('applications/${game.name}/thumb.png')`;
         gameContainer.appendChild(div);

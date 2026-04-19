f37088473f05a103b25097b6599560edd529f9d4
nexuster <coralpinklmao@outlook.com>
2026-04-19 13:37:27 -0400

    kirkules

--- console/app.js
+++ console/app.js
@@ -1,21 +1,17 @@
-var gcollection = document.getElementById("game_browser");
-display_games();
+const games = [
+    { name: "html_test"},
+    { name: "pico_consl_test"}
+];
 
 function display_games() {
-    fetch("console/applications")
-        .then(response => response.json())
-        .then(data => {
-            const files = data.files;
-            for (let i = 0; i < files.length; i++) {
-                const folder = files[i];
-                var app = document.createElement("div");
-                app.className = "app";
-                
-                const thumbPath = `console/applications/${folder}/thumb.png`;
-                app.style.backgroundImage = `url('${thumbPath}')`;
-                
-                gcollection.appendChild(app);
-            }
-        })
-        .catch(error => console.error("Error fetching applications:", error));
+    const gameContainer = document.getElementById("game_browser");
+    
+    for (const game of games) {
+        const div = document.createElement("div");
+        div.className = "app";
+        div.style.backgroundImage = `url(console/applications/'${game.name}/thumb.png')`;
+        gameContainer.appendChild(div);
+    }
 }
+
+display_games();
